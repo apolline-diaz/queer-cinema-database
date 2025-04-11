57ae66ba6d87efe8ce7adc72633869e50c578433
apolline-diaz <apolline.diaz@gmail.com>
2025-04-11 18:32:41 +0200

    add resend api key and update contact route - create about page

---
+++ app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <div className="px-10 py-5">
+      <h2 className="text-rose-500 text-xl mb-6">À propos</h2>
+      <p className="text-sm font-light sm:w-3/4 w-full mb-4">
+        Ce site est né du souhait d&apos;archiver le compte Instagram
+        <a
+          className="text-rose-500 px-1 hover:underline"
+          href="https://www.instagram.com/lawrens_shyboi/"
+        >
+          @lawrens_shyboi
+        </a>
+        que j&apos;ai créé en fin 2019 et que j&apos;ai géré jusqu&apos;en 2023.
+        J&apos;y partageais des références de films, de séries et
+        d&apos;émissions télé qui mettaient en scène des personnes LGBTQI+ ou
+        traitaient de sujets multiples entourant ces communautés.
+        <br /> Je crois que les images ont beaucoup à nous dire de
+        l&apos;histoire qui nous précède et du présent dans lequel nous vivons.
+        Alors montrer la richesse et la diversité des films qui existent et des
+        sujets qui les entourent me semblait crucial.
+        <br /> Afin de préserver la multitude de références collectés et
+        partagés au fil du temps grâce aux communautés (LGBTQI+ et
+        d&apos;autres), j&apos;ai réfléchi à une manière plus pérenne de
+        préserver la connaissance de ces films.
+        <br /> Développer un site avec une base de données propre dédiée à ces
+        films m&apos;est apparu comme la solution la plus viable.
+      </p>
+    </div>
+  );
+}
