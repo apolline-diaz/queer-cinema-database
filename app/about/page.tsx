6fed849f3c4c4ef5876eafb3b14e48d1a9f62c39
apolline-diaz <apolline.diaz@gmail.com>
2025-04-11 19:28:12 +0200

    update about text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,30 +1,91 @@
+import Link from "next/link";
+
 export default function About() {
   return (
-    <div className="px-10 py-5">
+    <div className="px-10 py-5 mb-5">
       <h2 className="text-rose-500 text-xl mb-6">À propos</h2>
-      <p className="text-sm font-light sm:w-3/4 w-full mb-4">
-        Ce site est né du souhait d&apos;archiver le compte Instagram
-        <a
-          className="text-rose-500 px-1 hover:underline"
-          href="https://www.instagram.com/lawrens_shyboi/"
-        >
-          @lawrens_shyboi
-        </a>
-        que j&apos;ai créé en fin 2019 et que j&apos;ai géré jusqu&apos;en 2023.
-        J&apos;y partageais des références de films, de séries et
-        d&apos;émissions télé qui mettaient en scène des personnes LGBTQI+ ou
-        traitaient de sujets multiples entourant ces communautés.
-        <br /> Je crois que les images ont beaucoup à nous dire de
-        l&apos;histoire qui nous précède et du présent dans lequel nous vivons.
-        Alors montrer la richesse et la diversité des films qui existent et des
-        sujets qui les entourent me semblait crucial.
-        <br /> Afin de préserver la multitude de références collectés et
-        partagés au fil du temps grâce aux communautés (LGBTQI+ et
-        d&apos;autres), j&apos;ai réfléchi à une manière plus pérenne de
-        préserver la connaissance de ces films.
-        <br /> Développer un site avec une base de données propre dédiée à ces
-        films m&apos;est apparu comme la solution la plus viable.
-      </p>
+      <div className="sm:w-3/4 w-full">
+        <p className="text-sm font-light  mb-4">
+          Ce site est né du souhait d&apos;archiver le compte Instagram
+          <a
+            className="text-rose-500 px-1 hover:underline"
+            href="https://www.instagram.com/lawrens_shyboi/"
+          >
+            @lawrens_shyboi
+          </a>
+          que j&apos;ai créé en fin 2019 et que j&apos;ai géré jusqu&apos;en
+          2023. J&apos;y partageais des références de films, de séries et
+          d&apos;émissions télé qui mettaient en scène des personnes LGBTQI+ ou
+          traitaient de sujets multiples entourant ces communautés.
+          <br /> Je crois que les images ont beaucoup à nous dire de
+          l&apos;histoire qui nous précède et du présent dans lequel nous
+          vivons. Alors montrer la richesse et la diversité des films qui
+          existent et des sujets qui les entourent semblait essentiel.
+          <br /> Afin de préserver la multitude de références collectés et
+          partagés au fil du temps grâce aux communautés (LGBTQI+ et
+          d&apos;autres), j&apos;ai réfléchi à une manière plus pérenne de
+          préserver la connaissance de ces films. Développer un site avec une
+          base de données propre dédiée à ces films m&apos;est apparu comme la
+          solution la plus viable.
+        </p>
+        <h3 className="font-bold mb-3">Le choix des films</h3>
+        <p className="text-sm mb-3">
+          Cette base de données n&apos;a pas pour objectif d'&apos;être
+          exhaustive et d&apos;inclure toutes les oeuvres existantes. Elle
+          provient de recherches personnelles et collectives. L&apos;intérêt est
+          de présenter des oeuvres qui portent un certain travail
+          cinématographique, un discours, ou faisant partie d&apos;une certaine
+          histoire des communautés LGBQTI+ à travers le monde.
+          <br /> Ce projet est né en France et donc le travail de recherche se
+          fait depuis cette localisation et ce point de vue.
+          <br /> Mon intérêt se porte surtout sur les oeuvres datés, avec un
+          intérêt plus fort pour les années 90 et 2000.
+          <br />
+          <span className="italic font-light text-xs">
+            J&apos;ai exclu les films érotiques et pornographiques mainstream.
+          </span>
+        </p>
+        <h3 className="font-bold mb-3">Pourquoi indexer les films?</h3>
+        <p className="text-sm ">
+          L&apos;indexation permet une recherche plus spécifique des films. Face
+          à la masse de productions sur les sujets LGBTQI+ qui existe
+          aujourd&apos;hui, il est difficile de trouver une oeuvre qui
+          correspond à nos envies ou à des sujets qui nous intéressent. <br />
+          J&apos;ai donc choisi d&apos;indexer les films en leur associant des
+          mots-clés et en utilisant d&apos;autres informations clés (le genre,
+          l&apos;année de sortie, le pays de production, lea réalisateur-ice, le
+          type).
+          <br />
+          Dans la page Catalogue, vous trouverez tous les films de la base de
+          données. Ils sont classés du plus récent ajout au plus ancien.
+          <br /> Il y a deux modes de recherche :
+          <ul>
+            <li>
+              - Recherche simple : tapez un titre de film ou tapez un mot-clés
+            </li>
+            <li>
+              - Recherche avancée : choisissez un critère de recherche ou
+              plusieurs combinés afin de trouver des résultats plus précis )
+            </li>
+          </ul>
+          <br />
+        </p>
+        <h3 className="font-bold mb-3">Le choix des mots-clé</h3>
+        <p className="text-sm">
+          Afin de mieux répertorier les films, j&apos;ai choisi de leur associer
+          des mots-clé qui permettent à l&apos;utilisateur d&apos;avoir une
+          vision globale des sujets dotn traite les productions.
+          <br />
+          J&apos;ai surtout mis en avant les thématiques, l&apos;identité et
+          l&apos;orientation sexuelle/romantique des personnages, et celles avec
+          qui ils flirtent. J&apos;ai intégré les expériences de vie, les
+          formats et genres cinématographiques, les régions géographiques.
+          <br />
+          J&apos;ai choisi de ne pas identifier les réalisateur-ices sur leur
+          identité de genre, orientation ou profession car les informations ne
+          peuvent pas être vérifiées et elles peuvent évoluer dans le temps.
+        </p>
+      </div>
     </div>
   );
 }
