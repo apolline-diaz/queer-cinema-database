8b67b2dd9cc2cd1f6978c635517a0f8330410162
apolline-diaz <apolline.diaz@gmail.com>
2025-04-14 19:02:00 +0200

    add fill

--- app/components/home-card.tsx
+++ app/components/home-card.tsx
@@ -27,7 +27,6 @@ export default function HomeCard({
               src={getImageUrl(image_url)}
               alt={title}
               fill
-              loading="lazy"
               className="object-cover rounded-xl transition-transform duration-700 ease-in-out group-hover:scale-110 group-hover:brightness-50 group-hover:rounded-xl"
               sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
             />
