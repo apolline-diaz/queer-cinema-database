a0558af2b36f4636ed0177db8fb3e9782cd4c25d
apolline-diaz <apolline.diaz@gmail.com>
2025-05-14 09:31:15 +0200

    add data type to movies data in move page

--- app/movies/[slug]/page.tsx
+++ app/movies/[slug]/page.tsx
@@ -41,20 +41,6 @@ export default async function Page({ params }: Props) {
         />
         <div className="absolute bottom-0 bg-black bg-opacity-30 w-full h-full text-white p-10 flex justify-between items-end">
           <div className="flex flex-row justify-between items-center w-full">
-            <div className="flex flex-col">
-              <h2 className="text-4xl mb-2 font-bold uppercase">
-                {movie.title}
-              </h2>
-              <h2 className="text-lg font-light ">
-                {movie.directors?.length > 0 && (
-                  <span>
-                    {movie.directors
-                      .map((director) => director.name)
-                      .join(", ")}
-                  </span>
-                )}
-              </h2>
-            </div>
             <div className="">
               {session && (
                 <ClientMovieActions
@@ -66,41 +52,65 @@ export default async function Page({ params }: Props) {
           </div>
         </div>
       </div>
-      <div className="p-10 gap-3 flex flex-col">
-        <div className="flex flex-col font-light">
-          <span className="font-semibold  text-rose-500">
-            {[
-              movie.countries?.map((country) => country.name).join(", "),
-              movie.release_date,
-              movie.runtime ? `${movie.runtime}${durationText}` : null,
-              movie.type,
-              movie.language,
-            ]
-              .filter(Boolean)
-              .join(" • ")}
-          </span>
-        </div>
-        <div className="font-bold mb-2 ">
-          {movie?.genres?.length > 0 && (
-            <span className="font-light text-rose-500 pr-2">
-              {movie.genres.map((genre) => genre.name).join(", ")}
+      <div className="p-10 text-black flex flex-col font-light gap-3">
+        <h2 className="text-4xl mb-2 font-light uppercase text-rose-500">
+          {movie.title}
+        </h2>
+        <h2 className="font-light text-lg">
+          {movie.directors?.length > 0 && (
+            <span>
+              {movie.directors.map((director) => director.name).join(", ")}
             </span>
           )}
-        </div>
-        <p className="pt-6 pb-4 font-light text-black border-t border-black">
-          {movie.description}
-        </p>
-        <div className="font-bold flex items-center flex-wrap gap-2">
-          {movie.keywords?.map((keyword) => (
-            <Link
-              key={keyword.id}
-              href={`/movies?keywordIds=${encodeURIComponent(keyword.id.toString())}&searchMode=form`}
-            >
-              <span className="font-light text-sm rounded-full border border-rose-500 text-rose-500 px-2 mr-1 py-1 hover:bg-rose-500 hover:text-white hover:border-rose-500 hover:cursor-pointer">
-                {keyword.name}
+        </h2>
+        <div className="flex flex-col sm:flex-row">
+          <div className="min-w-[150px] uppercase grid sm:grid-cols-1 grid-cols-3 gap-4 font-light mt-4">
+            <div className="text-sm ">
+              <h3 className="mb-1 text-rose-500">Pays</h3>
+              <span>
+                {movie.countries?.map((country) => country.name).join(", ")}
               </span>
-            </Link>
-          ))}
+            </div>
+            <div className="text-sm">
+              <h3 className="mb-1 text-rose-500">Année</h3>
+              <span>{movie.release_date}</span>
+            </div>
+            <div className="text-sm">
+              <h3 className=" mb-1 text-rose-500">Durée</h3>
+              <span>{movie.runtime} min</span>
+            </div>
+            <div className="text-sm">
+              <h3 className=" mb-1 text-rose-500">Format</h3>
+              <span>{movie.type}</span>
+            </div>
+            <div className="text-sm">
+              <h3 className=" mb-1 text-rose-500">Langue(s)</h3>
+              <span>{movie.language}</span>
+            </div>
+            {movie.genres?.length > 0 && (
+              <div className="text-sm">
+                <h3 className="mb-1 text-rose-500">Genre(s)</h3>
+                <span>
+                  {movie.genres.map((genre) => genre.name).join(", ")}
+                </span>
+              </div>
+            )}
+          </div>
+          <div>
+            <p className="mt-4 text-lg">{movie.description}</p>
+            <div className="font-bold flex items-center flex-wrap py-3">
+              {movie.keywords?.map((keyword) => (
+                <Link
+                  key={keyword.id}
+                  href={`/movies?keywordIds=${encodeURIComponent(keyword.id.toString())}&searchMode=form`}
+                >
+                  <span className="font-light text-md rounded-full border border-rose-500 text-rose-500 px-2 mr-1 py-1 hover:bg-rose-500 hover:text-white hover:border-rose-500 hover:cursor-pointer">
+                    {keyword.name}
+                  </span>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
