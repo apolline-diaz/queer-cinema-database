f70af0cec2de756003571d05acf9b8ab69c4cdb0
apolline-diaz <apolline.diaz@gmail.com>
2025-05-24 16:54:16 +0200

    distinct to see and discover sections

--- app/page.tsx
+++ app/page.tsx
@@ -25,52 +25,48 @@ export default async function Home() {
             />
           ))}
         </div>
+        <h2 className="text-rose-500 px-10 py-5 right-5 text-3xl font-extrabold font-raleway">
+          À voir
+        </h2>
 
         {/* First movie highlight */}
         {featuredLatestMovie && (
-          <div className="w-full mb-5 relative overflow-hidden featured-latest-movie">
-            <Link href={`/movies/${featuredLatestMovie.id}`}>
-              <div className="relative h-[90vh] w-full">
+          <div className="w-full sm:flex-row flex flex-col mb-5 relative overflow-hidden featured-latest-movie">
+            <div className="h-[50vh] sm:w-1/2 w-full sm:pl-10 pl-none">
+              <Link href={`/movies/${featuredLatestMovie.id}`}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src={getImageUrl(featuredLatestMovie.image_url || "")}
                   alt={featuredLatestMovie.title}
                   className="w-full h-full object-cover"
                 />
-                <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/80 via-black/50 to-transparent">
-                  <div className="absolute top-5 right-5 text-white">
-                    <h2 className="text-rose-500 text-5xl p-5  font-extrabold font-raleway">
-                      Dernier ajout
-                    </h2>
-                  </div>
-                  <div className="sm:w-3/4 w-full p-10 text-white">
-                    <h3 className="text-4xl uppercase font-bold mb-2">
-                      {featuredLatestMovie.title}
-                    </h3>
-                    <span className="inline-block text-md font-light">
-                      {featuredLatestMovie?.movies_directors
-                        ?.map((item) => item.directors.name)
-                        .join(", ") || "Réalisateur inconnu"}
-                    </span>{" "}
-                    •{" "}
-                    <span className="inline-block mb-1 text-md font-light">
-                      {featuredLatestMovie.release_date || ""}
-                    </span>
-                    <p className="line-clamp-6 text-md font-extralight">
-                      {featuredLatestMovie.description ||
-                        "Pas de description disponible"}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
+            <div className="sm:px-10 sm:py-0 p-10 sm:w-1/2 text-black">
+              <h3 className="text-2xl uppercase font-bold">
+                {featuredLatestMovie.title}
+              </h3>
+              <span className="inline-block text-md font-light">
+                {featuredLatestMovie?.movies_directors
+                  ?.map((item) => item.directors.name)
+                  .join(", ") || "Réalisateur inconnu"}
+              </span>{" "}
+              •{" "}
+              <span className="inline-block mb-1 text-md font-light">
+                {featuredLatestMovie.release_date || ""}
+              </span>
+              <p className="line-clamp-6 text-md font-extralight">
+                {featuredLatestMovie.description ||
+                  "Pas de description disponible"}
+              </p>
+            </div>
           </div>
         )}
         <div className="pl-10">
           <div className="flex flex-col mb-5">
             <div className="flex justify-between items-center pr-10 mb-4">
-              <h2 className="text-lg font-semibold  text-rose-500">
-                plus d&apos;archives{" "}
+              <h2 className="text-3xl font-semibold  text-rose-500">
+                Découvrir
               </h2>
               <Link
                 href="/movies"
