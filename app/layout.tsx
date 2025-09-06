b4aabd656600f77a286d229858bfbcc736308451
apolline-diaz <apolline.diaz@gmail.com>
2025-09-06 13:02:38 +0200

    update url favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,11 +54,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/assets/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/assets/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    shortcut: "/assets/favicon.ico",
+    apple: "/assets/apple-touch-icon.png",
   },
 };
 
